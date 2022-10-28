ab2491a3ec594af63a99fb1af20fc06a5560562f
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2022-10-28 14:02:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const { createApp } = Vue
+
+createApp({
+        data() {
+        return {
+            postsList: [],
+            userInput: ""
+            /* credentials: "" */
+        }
+    },
+    methods: {
+        addLike(post) {
+            if(post.likes) {
+                post.likes++
+            } else {
+                post.likes = 1
+            }
+        },
+        addComment(event, post) {
+            let { value } = event.target.elements[0]
+            const comment = {
+                id: post.comments ? post.comments.length : 1,
+                body: value
+            }
+            if (post.comments) {
+                post.comments.push(comment)
+            } else {
+                post.comments = [comment]
+            }
+            console.log(event.target.elements[0].value)
+            event.target.elements[0].value = ""
+
+        },
+    },
+    mounted() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => this.postsList.push(...data))
+    }
+}).mount('#app')
